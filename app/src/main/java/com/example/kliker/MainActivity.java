e538cff632e71439fa2a1c5efc1005789de31a90
maria <mariapivnova@yandex.ru>
2022-03-18 10:10:37 +0300

    Nerve counter

--- app/src/main/java/com/example/kliker/MainActivity.java
+++ app/src/main/java/com/example/kliker/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void onClickBtnAddStudents(View view) {
         counter++;
         TextView counterView = (TextView)findViewById(R.id.txt_counter);
